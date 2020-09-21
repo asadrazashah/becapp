dce6b83f54cbe50dff51ced0056abba2c2da19c1
Asad Raza <basitsattar850@gmail.com>
2020-09-21 22:03:19 +0500

    added forgot password screen

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./screens/Login";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ForgotPassword from './screens/ForgotPassword';
 function Routes() {
     return (
         <Router>
             <Navbar />
             <Switch>
                 <Route exact path="/" component={Login} />
+                <Route exact path="/forgot-password" component={ForgotPassword} />
             </Switch>
             <Footer />
         </Router>
---
+++ src/screens/ForgotPassword.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import InputIcon from "../components/InputIcon";
+import ForgotImg from "../assets/forgot_password.png";
+import ThemeButton from "../components/ThemeButton";
+import "../styles/screens/login.scss"
+
+const ForgotPassword: React.FC = () => {
+    return <div className="wrapAuthScreens">
+        <div className="login">
+            <Grid container>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <div className="loginImg">
+                        <img src={ForgotImg} alt="fotgot password" className="img" />
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <div className="wrapperForm">
+                        <h1 className="headingAuth">Forgot Password</h1>
+                        <InputIcon placeholder="Mobile Number" iconClass="iconPerson" type="email" style={{ marginTop: "70px" }} />
+
+                        <ThemeButton title="Send OTP" />
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    </div>
+}
+
+export default ForgotPassword;
