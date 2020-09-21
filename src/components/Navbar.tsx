0e0b114da9b894e6ecb11c77e6022003940f103b
Asad Raza <basitsattar850@gmail.com>
2020-09-21 21:51:56 +0500

    added logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Logo from "../assets/navbar_logo.png";
 import "../styles/components/navbar.scss";
 
 const Navbar: React.FC = () => {
     return <div className="containerNavbar">
-        <h2>BeeCapp</h2>
+        <div className="logoNav">
+            <img src={Logo} alt="logo nav" />
+        </div>
     </div>
 }
 export default Navbar;
