62b05a49bf59b4fe19ddfaa11741df888faa5ac4
Asad Raza <basitsattar850@gmail.com>
2020-09-22 19:13:08 +0500

    added testimonial component

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Avatar from "../assets/norm_avatar.png";
+import "../styles/components/testimonial.scss";
+
+interface IProps {
+    overlay?: string;
+}
+const Testimonial: React.FC<IProps> = ({ overlay }) => {
+    return <div className={`wrapperTestimonial ${overlay}`}>
+        <div className="testimonialContent">
+            <img src={Avatar} alt="testimonial" />
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+ tation ullamcorper suscipit lobortis nisl ut aliquip.</p>
+            <h4>Bushra Ansari</h4>
+        </div>
+    </div>
+}
+export default Testimonial;
--- src/screens/AboutUs.tsx
+++ src/screens/AboutUs.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DescriptionPage from "../components/DescriptionPage"
+import Testimonial from "../components/Testimonial";
 import "../styles/screens/about_us.scss"
 
 const AboutUs: React.FC = () => {
     return <div>
         <DescriptionPage title="ABOUT US" link="ABOUT" />
+        <Testimonial overlay="dark" />
     </div>
 
 }
