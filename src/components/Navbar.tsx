62545680eca3743ba34c19be638b14ca45ca9720
Asad Raza <basitsattar850@gmail.com>
2020-09-25 20:39:13 +0500

    added links path to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import ThemeButton from "./ThemeButton";
 import Logo from "../assets/logo.svg";
 import SearchIcon from "../assets/searchIcon.svg";
 import "../styles/components/navbar.scss";
 interface IProps {
     authenticated: boolean;
+    history: any;
 }
-const Navbar: React.FC<IProps> = ({ authenticated }) => {
+const Navbar: React.FC<IProps> = ({ authenticated, history }) => {
+    const postJob = () => {
+        history.push("/home/postjob")
+    }
     return <div className="containerNavbar">
         <img src={Logo} alt="logo nav" className="logo" />
         {authenticated && <div className="linksNavBar">
-            <Link to="/">Home</Link>
-            <Link to="/">About Us</Link>
-            <Link to="/">How it works</Link>
-            <Link to="/">Find Work</Link>
-            <Link to="/">My Jobs</Link>
+            <Link to="/home">Home</Link>
+            <Link to="/home/about">About Us</Link>
+            <Link to="/home/working">How it works</Link>
+            <Link to="/home/findwork">Find Work</Link>
+            <Link to="/home/myjobs">My Jobs</Link>
             <Link to="/">Reports</Link>
-            <Link to="/">Contact Us</Link>
+            <Link to="/home/contact">Contact Us</Link>
             <span><img src={SearchIcon} alt="search" /></span>
-            <ThemeButton title="Post a Job" borderRadius="withRadius" />
+            <ThemeButton title="Post a Job" borderRadius="withRadius" onClick={postJob} />
         </div>}
     </div>
 }
-export default Navbar;
+export default withRouter<any, any>(Navbar);
